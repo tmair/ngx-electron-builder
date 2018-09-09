5de14a882787e55cc48aed19eb9d37258bd393aa
Thomas Mair <thomas.mair86@gmail.com>
2018-09-09 22:39:21 +0200

    feat: Add watch mode for main process files
    Add a typescript watcher to watch for changes to the main process files. Start the electron process
    only after angular and the main process sources are compiled for the first time
    
    fix #1

--- src/electron-dev-server/index.ts
+++ src/electron-dev-server/index.ts
@@ -1,32 +1,41 @@
-import { BuilderConfiguration, BuildEvent } from '@angular-devkit/architect';
+import { Builder, BuilderConfiguration, BuildEvent } from '@angular-devkit/architect';
 import { DevServerBuilder as DevServerBuilderBase, DevServerBuilderOptions } from '@angular-devkit/build-angular';
 import { spawn } from 'child_process';
 import * as path from 'path';
 import { combineLatest, Observable } from 'rxjs';
-import { concatMap, map, share, take } from 'rxjs/operators';
+import { first, map, shareReplay, switchMap } from 'rxjs/operators';
 import { ElectronBuilderSchema } from '../electron/schema';
-import { compile, readConfigFile } from './typescript';
+import { compile, readConfigFile, watch } from '../typescript';
+import { ElectronServerBuilderOptions } from './schema';
 
-export class DevServerBuilder extends DevServerBuilderBase {
+export class DevServerBuilder extends DevServerBuilderBase
+  implements Builder<ElectronBuilderSchema> {
   run(
-    builderConfig: BuilderConfiguration<DevServerBuilderOptions>
+    builderConfig: BuilderConfiguration<ElectronServerBuilderOptions>
   ): Observable<BuildEvent> {
-    const angularBuilder = super.run(builderConfig).pipe(share());
     const config = this.getBuilderConfiguration(builderConfig);
-    const typescript = this.runTypescript(config).pipe(share());
+    const startWatcher: boolean = builderConfig.options.reloadOnChanges || false;
+
+    const angularBuilder = super.run(builderConfig).pipe(shareReplay(1));
+    const typescript = this.runTypescript(config, startWatcher).pipe(
+      shareReplay(1)
+    );
 
     const runElectron = combineLatest(
-      angularBuilder.pipe(take(1)),
-      typescript.pipe(take(1))
-    ).pipe(concatMap(e => this.runElectron(config)));
+      angularBuilder.pipe(first()),
+      typescript.pipe(first())
+    ).pipe(switchMap(e => this.runElectron(config)));
 
     return combineLatest(
-      // TODO watch mode for typescript
+      typescript,
       angularBuilder,
       runElectron,
-      (typescriptResult, angularBuilderResult) => {
+      (typescriptResult, angularBuilderResult, runElectronResult) => {
         return {
-          success: typescriptResult.success && angularBuilderResult.success
+          success:
+            typescriptResult.success &&
+            angularBuilderResult.success &&
+            runElectronResult.success
         };
       }
     );
@@ -35,7 +44,6 @@ export class DevServerBuilder extends DevServerBuilderBase {
   private runElectron(
     config: BuilderConfiguration<ElectronBuilderSchema>
   ): Observable<BuildEvent> {
-
     const electron = require('electron');
 
     return readConfigFile(config.options.electronProject).pipe(
@@ -61,12 +69,19 @@ export class DevServerBuilder extends DevServerBuilderBase {
   }
 
   private runTypescript(
-    config: BuilderConfiguration<ElectronBuilderSchema>
+    config: BuilderConfiguration<ElectronBuilderSchema>,
+    startWatcher: boolean = false
   ) {
-    return compile(config.options.electronProject);
+    if (startWatcher) {
+      return watch(config.options.electronProject);
+    } else {
+      return compile(config.options.electronProject);
+    }
   }
 
-  private getBuilderConfiguration( builderConfig: BuilderConfiguration<DevServerBuilderOptions>) {
+  private getBuilderConfiguration(
+    builderConfig: BuilderConfiguration<DevServerBuilderOptions>
+  ): BuilderConfiguration<ElectronBuilderSchema> {
     const [project, targetName, configuration] = (builderConfig.options
       .browserTarget as string).split(':');
     const targetSpec = {
@@ -79,7 +94,5 @@ export class DevServerBuilder extends DevServerBuilderBase {
     >(targetSpec);
   }
 }
-
-
 
 export default DevServerBuilder;
